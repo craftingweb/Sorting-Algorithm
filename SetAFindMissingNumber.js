143f1982345d1d8a7e6d556a823a9a6c6afa11ae
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2023-07-09 17:18:42 -0400

    Find A Missing Number

--- SetAFindMissingNumber.js
+++ SetAFindMissingNumber.js
@@ -1,3 +1,35 @@
 /*
 Problem Description: Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the missing number.
+
+Goal is to find missing number in a sorted array.
+Perform cyclic sort
+If the current number is within range and not in its correct position
+    Swap the current number with the number at its correct position
+    Else: Move to the next number
+Find the missing number
+   If the number at index i is not equal to i, it is the missing number
+   If no missing number is found, the missing number n
 */
+function findMissingNumber(nums) {
+  const n = nums.length;
+
+  let i = 0;
+  while (i < n) {
+    const correctIndex = nums[i];
+    if (nums[i] < n && nums[i] !== nums[correctIndex]) {
+      [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+    } else {
+      i++;
+    }
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (nums[i] !== i) {
+      return i;
+    }
+  }
+
+  return n;
+}
+
+module.exports = { findMissingNumber };
--- SetAFindMissingNumber.test.js
+++ SetAFindMissingNumber.test.js
@@ -1,8 +1,37 @@
 // importing file with algorithm
-// const missingNumber = require("./setAFindMissingNumber");
+const missingNumber = require("./SetAFindMissingNumber");
 // destructiring algorithm from the file for testing
-// let { sum } = missingNumber;
-// unit test for algorithm
-// test("checking algorithm", () => {
-//   expect(sum(1, 4)).toBe(5);
-// });
+let { findMissingNumber } = missingNumber;
+// test suit for a missing mumber
+describe("Find a missing mumber in a sorted array", () => {
+  // unit tests for a missing mumber
+  it("should return the missing number when it is in the middle of the array", () => {
+    const nums = [0, 2, 3, 4, 5];
+    expect(findMissingNumber(nums)).toBe(1);
+  });
+
+  it("should return the missing number when it is at the beginning of the array", () => {
+    const nums = [1, 2, 3, 4, 5];
+    expect(findMissingNumber(nums)).toBe(0);
+  });
+
+  it("should return the missing number when it is at the end of the array", () => {
+    const nums = [0, 1, 2, 3, 4];
+    expect(findMissingNumber(nums)).toBe(5);
+  });
+
+  it("should return the missing number in an array with only one element", () => {
+    const nums = [0];
+    expect(findMissingNumber(nums)).toBe(1);
+  });
+
+  it("should return the missing number in an array with two elements", () => {
+    const nums = [0, 1];
+    expect(findMissingNumber(nums)).toBe(2);
+  });
+
+  it("should return 0 when the array is empty", () => {
+    const nums = [];
+    expect(findMissingNumber(nums)).toBe(0);
+  });
+});
