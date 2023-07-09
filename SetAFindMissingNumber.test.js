95cd675e373ea179e7093938422df75409457918
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2023-07-09 16:58:19 -0400

    issue fixed, happen because of broken packages, now jest is working

---
+++ SetAFindMissingNumber.test.js
@@ -0,0 +1,8 @@
+// importing file with algorithm
+// const missingNumber = require("./setAFindMissingNumber");
+// destructiring algorithm from the file for testing
+// let { sum } = missingNumber;
+// unit test for algorithm
+// test("checking algorithm", () => {
+//   expect(sum(1, 4)).toBe(5);
+// });
