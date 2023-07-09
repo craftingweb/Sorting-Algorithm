4b2e84dd251f0330a69d9e74bd8eb8a9844e3925
Hfear <hannahbfearns@gmail.com>
2023-07-09 17:38:29 -0400

    set mismatch code

---
+++ SetA3-SetMismatch.js
@@ -0,0 +1,30 @@
+// You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+// You are given an integer array nums representing the data status of this set after the error.
+// Find the number that occurs twice and the number that is missing and return them in the form of an array.
+
+// Constraints:
+// 2 <= nums.length <= 104
+// 1 <= nums[i] <= 104
+
+//using cyclic sort
+function SetMismatch(nums)
+{
+    for(let currentIndex = 0; currentIndex < nums.length; currentIndex++)
+    {
+        const value = nums[i]; 
+        const correctIndex = value - 1; 
+
+        //if current val is not in correct index
+        if(currentIndex != correctIndex)
+        {
+            //if the number already at the correct index m
+            //matches the one that needs to be swapped,,
+            // there is a duplicate
+            if(nums[currentIndex] === nums[correctIndex])
+            {
+                return  [nums[currentIndex], nums[currentIndex]+1];
+            }
+
+        }
+    }
+}
