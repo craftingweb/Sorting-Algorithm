e9d532a668239f4a9d23289041e02702604d8e71
Hfear <hannahbfearns@gmail.com>
2023-07-09 23:54:57 -0400

    more tests

--- SetB1-RemoveDuplicatesfromSortedArray.js
+++ SetB1-RemoveDuplicatesfromSortedArray.js
@@ -11,41 +11,46 @@
 
 //using pointers cause you cant use cyclic search with negative integers 
 
-// class Node {
-//     constructor(value, next=null) {
-//       this.value = value;
-//       this.next = next;
-//     }
-//   }
+class Node {
+  constructor(value, next=null) {
+    this.value = value;
+    this.next = next;
+  }
+}
 
 
-  function RemoveDuplicates(nums)
+function RemoveDuplicates(nums)
+{
+  //if the array is empty return null
+  if(nums.length === 0)
   {
-    //setting our pointers to the start of array 
-    let fast = 0;
-    let slow = 0; 
+    return null; 
+  }
+  //setting our pointers to the start of array 
+  let fast = head;
+  let slow = head; 
 
-    //while not at the end of array 
-    while(fast < nums.length)
-    {
-        //updating pointers
-        slow ++;
-        fast += 2;
+  //while not at the end, or 1 cycle has completed
+  while((fast !== null && fast.next !== null) || (fast === slow))
+  {
+      //updating pointers
+      slow = slow.next;
+      fast = fast.next.next;
 
-        //fast should be equal to slow * 2 ,, if its not then there is a duplicate
-        if(nums[fast] != (nums[slow] * 2))
-        {
-            //we want to remove the duplicate 
-            nums = nums.splice((fast+ 1), 1);
-            
-        }
+      i = 1; 
+      i++; 
 
-    
-    }
+      //fast should be equal to slow + 2 ,, if its not then there is a duplicate
+      if(fast.value != (slow.value + i))
+      {
+          //we want to remove the duplicate
+          //remove inbetween fast and slow and move on 
+          nums = nums.splice((fast.value - 1), 1);
+          
+      }
 
-    return nums.length; 
+  
   }
 
-  module.exports = {
-    RemoveDuplicates,
-  };
+  return nums.length; 
+}
--- SetB1.test.js
+++ SetB1.test.js
@@ -1,5 +1,21 @@
 const {RemoveDuplicates} = require("./SetB1-RemoveDuplicatesfromSortedArray")
 
-test('RemoveDuplicates([1,2,2,3] returns 2', () =>{
-    expect(RemoveDuplicates([1,2,2,3])).toBe(2);
+test('RemoveDuplicates([] returns null', () =>{
+    expect(RemoveDuplicates([])).toBe(null);
 })
+
+// test("RemoveDuplicates([1, 1, 2]) returns 2", () => {
+//   expect(RemoveDuplicates([1, 1, 2])).toBe(2);
+// });
+
+// test("RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]) returns 5", () => {
+//   expect(RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])).toBe(5);
+// });
+
+// test("RemoveDuplicates([-2, -2, -1, 0, 1, 1, 2, 2]) returns 5", () => {
+//   expect(RemoveDuplicates([-2, -2, -1, 0, 1, 1, 2, 2])).toBe(5);
+// });
+
+// test("RemoveDuplicates([1, 1, 1, 1, 1]) returns 1", () => {
+//   expect(RemoveDuplicates([1, 1, 1, 1, 1])).toBe(1);
+// });
