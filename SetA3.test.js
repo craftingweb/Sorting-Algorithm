739c27633488b8726aeb0c3a8687681ef0c7e2ab
Hfear <hannahbfearns@gmail.com>
2023-07-10 08:52:20 -0400

    fixed b1

--- SetA3.test.js
+++ SetA3.test.js
@@ -11,3 +11,11 @@ test('SetMismatch([1,2,3,3] returns [3,4]', () =>{
 test('Returns null if theres no mismatch', () =>{
     expect(SetMismatch([1,2,3,4])).toBe(null);
 })
+
+// test('SetMismatch([2,2,3,4]) returns [1,2]', () =>{
+//     expect(SetMismatch([2,2,3,4])).toStrictEqual([1,2]);
+// });
+
+// test('SetMismatch([1,2,3,4,4,5,6]) returns [4,5]', () =>{
+//     expect(SetMismatch([1,2,3,4,4,5,6])).toStrictEqual([4,5]);
+// });
